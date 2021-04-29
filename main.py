85994a71418b8822a50ce24f83f44ff203328bb5
Adam Perkowski <adas1per@protonmail.com>
2021-04-29 11:04:27 +0200

    lingosik

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import pyautogui
 from time import sleep
 from PIL import Image
 import cv2
-import pytesserac
+import pytesseract
 
 pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 tk = Tk()
 tk.title("Lingos Hacker")
 
-dawaj = Label(tk, text="ILEE??!!! lingos'ów")
-ilee = Entry(tk)
-notka = Label(tk, text='Lingos Hacker by Adam Perkowski (APMP Studios).\nAll Rights Reserved.')
-dawaj.pack()
-ilee.pack()
-notka.pack()
+#dawaj = Label(tk, text="ILEE??!!! lingos'ów")
+#ilee = Entry(tk)
+#notka = Label(tk, text='Lingos Hacker by Adam Perkowski (APMP Studios).\nAll Rights Reserved.')
+#ileeBut = 
+#dawaj.pack()
+#ilee.pack()
+#notka.pack()
 
-for i in range(ilee)
+ilee = 1
+sleep(3)
+
+for i in range(ilee):
     skrin = pyautogui.screenshot(region=(725,386, 532, 31))
-    skrin.save('lingos - nie usuwac.png')
-    img = cv2.imread('lingos - nie usuwac.png')
-    img = cv2.cvtColor(img,cv2.COLOR_BRG2RGB)
+    skrin.save('lingos.png')
+    img = cv2.imread('lingos.png')
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     textBef = pytesseract.image_to_string(img)
 
     pyautogui.typewrite(textBef)
