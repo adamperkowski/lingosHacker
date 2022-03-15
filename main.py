11cf1da5c212b3029e28a075c4f2d3a04dcd0b66
Adam Perkowski <adas1per@protonmail.com>
2022-03-15 17:51:44 +0100

    Odnowienie
    +  7b dict
    -  'lingos - nie usuwac.png'
    •  main.py

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-from tkinter import *
 import pyautogui
 from time import sleep
 from PIL import Image
 import cv2
 import pytesseract
 
-pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-tk = Tk()
-tk.title("Lingos Hacker")
-
-#dawaj = Label(tk, text="ILEE??!!! lingos'ów")
-#ilee = Entry(tk)
-#notka = Label(tk, text='Lingos Hacker by Adam Perkowski (APMP Studios).\nAll Rights Reserved.')
-#ileeBut = 
-#dawaj.pack()
-#ilee.pack()
-#notka.pack()
+dictionary = open("7b.dict", 'r')
+readdict = dictionary.read()
 
 ilee = 1
 sleep(3)
 
 for i in range(ilee):
-    skrin = pyautogui.screenshot(region=(725,386, 532, 31))
+    skrin = pyautogui.screenshot(region=(690,250, 750,62))
     skrin.save('lingos.png')
     img = cv2.imread('lingos.png')
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    textBef = pytesseract.image_to_string(img)
+    textBef = pytesseract.image_to_string(img, lang='pol')
+    print(textBef)
+    print(readdict.find(textBef))
 
-    pyautogui.typewrite(textBef)
-    pyautogui.press('enter')
-    sleep(1.5)
-    pyautogui.press('enter')
-
+    if textBef in readdict:
+        pyautogui.typewrite('cheetah')
+        pyautogui.press('enter')
+        sleep(1.5)
+        pyautogui.press('enter')
 
 # https://www.youtube.com/watch?v=6DjFscX4I_c
 # Trzeba na komputerze klienta zainstalowac tesseract i polski jezyk rozpoznawania
