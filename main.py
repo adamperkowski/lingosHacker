c8980cd6fe868bbb2b7db29116ddfcddd54f427f
Adam Perkowski <adas1per@protonmail.com>
2022-03-30 18:46:56 +0200

    GitHub username update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ print(' [ lingos.pl cheat tool ]')
 sleep(0.2)
 print(' [ Created by Adaśko ]')
 sleep(0.2)
-print(' [ Discord: 𝕙𝕒𝕜𝕚𝕖𝕣 𝕜𝕝𝕒𝕜𝕚𝕖𝕣#0232 ]')
+print(' [ Discord: adask00#0232 ]')
 sleep(0.2)
 print(' [ Snapchat: adaskotodebil ]')
 sleep(0.2)
@@ -56,21 +56,19 @@ for i in range(ilee):
     textBef = pytesseract.image_to_string(img, lang='pol')
 
     if textBef in linesdict:
+    #    if linesdict.count(textBef) > 1:
+
         print(f'\n{textBef}')
-        for line in linesdict:
-            if str(line) == str(textBef):
-                pyautogui.typewrite(linesdict[x+1])
-                pyautogui.press('enter')
-                sleep(1.5)
-                pyautogui.press('enter')
-                break
-            #else:
-            #    print('nie ma')
-            x = x + 1
+        
+        pyautogui.typewrite(linesdict[linesdict.index(textBef)+1])
+        pyautogui.press('enter')
+        sleep(1.5)
+        pyautogui.press('enter')
 
     elif textBef == 'Nowe słowo od nauczyciela!\n':
         print('''\n [*]  Nowe słowo!
  [*]  Zostanie dodane do bazy danych automatycznie.''')
+
         #1 ----------------------------------------------------
         skrin = pyautogui.screenshot(region=(703,384, 593,30))
         skrin.save('lingos.png')
@@ -81,26 +79,32 @@ for i in range(ilee):
         textBef = pytesseract.image_to_string(img, lang='pol')
         print(f'\n{textBef}')
 
-        dictionary.write(f'{textBef}')
-        #2 ----------------------------------------------------
-        skrin = pyautogui.screenshot(region=(703,355, 655,30))
-        skrin.save('lingos.png')
+        if textBef not in linesdict:
 
-        img = cv2.imread('lingos.png')
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    
-        textBef = pytesseract.image_to_string(img, lang='pol')
-        print(f'\n{textBef}')
+            dictionary.write(f'{str(textBef)}')
+            #2 ----------------------------------------------------
+            skrin = pyautogui.screenshot(region=(703,355, 655,30))
+            skrin.save('lingos.png')
 
-        dictionary.write(f'{textBef}')
+            img = cv2.imread('lingos.png')
+            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-        pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
-        sleep(0.9)
+            textBef = pytesseract.image_to_string(img, lang='pol')
+            print(f'\n{textBef}')
 
-        i = i - 1
-        dictionary.close()
-        dictionary = open("lingos.dict", "r+", encoding='utf8')
-        linesdict = dictionary.readlines()
+            dictionary.write(f'{str(textBef)}')
+
+            pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
+            sleep(0.9)
+
+            i = i - 1
+            dictionary.close()
+            dictionary = open("lingos.dict", "r+", encoding='utf8')
+            linesdict = dictionary.readlines()
+        else:
+            pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
+            sleep(0.9)
+            i = i - 1
 
     else:
         print(f'\n{textBef}')
