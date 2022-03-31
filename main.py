7eec510c468a6f9068159b35733c113ed2381932
Adam Perkowski <adas1per@protonmail.com>
2022-03-31 15:59:40 +0200

    Remove else: from while:

--- main.py
+++ main.py
@@ -49,102 +49,101 @@ print(' [*]  Zmień okno na Firefoxa i naduś F1')
 
 while is_pressed('f1') != True:
     continue
-else:
-    #--------------------------------------------------------------------------MAIN--------------------------------------------------------------------------
-    for i in range(ilee):
-        x = int()
+
+#--------------------------------------------------------------------------MAIN--------------------------------------------------------------------------
+for i in range(ilee):
+    x = int()
+    sleep(1.5)
+
+    skrin = pyautogui.screenshot(region=(690,262, 750,30))
+    skrin.save('lingos.png')
+
+    img = cv2.imread('lingos.png')
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    textBef = pytesseract.image_to_string(img, lang='pol')
+
+    if textBef in linesdict:
+    #    if linesdict.count(textBef) > 1:
+
+        print(f'\n{textBef}')
+        
+        pyautogui.typewrite(linesdict[linesdict.index(textBef)+1])
+        pyautogui.press('enter')
         sleep(1.5)
-    
-        skrin = pyautogui.screenshot(region=(690,262, 750,30))
+        pyautogui.press('enter')
+
+    elif textBef == 'Nowe słowo od nauczyciela!\n':
+        print('''\n [*]  Nowe słowo!
+ [*]  Zostanie dodane do bazy danych automatycznie.''')
+
+        #1 ----------------------------------------------------
+        skrin = pyautogui.screenshot(region=(703,384, 593,30))
         skrin.save('lingos.png')
-    
+
         img = cv2.imread('lingos.png')
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     
         textBef = pytesseract.image_to_string(img, lang='pol')
-    
-        if textBef in linesdict:
-        #    if linesdict.count(textBef) > 1:
-    
-            print(f'\n{textBef}')
-            
-            pyautogui.typewrite(linesdict[linesdict.index(textBef)+1])
-            pyautogui.press('enter')
-            sleep(1.5)
-            pyautogui.press('enter')
-    
-        elif textBef == 'Nowe słowo od nauczyciela!\n':
-            print('''\n [*]  Nowe słowo!
-     [*]  Zostanie dodane do bazy danych automatycznie.''')
-    
-            #1 ----------------------------------------------------
-            skrin = pyautogui.screenshot(region=(703,384, 593,30))
+        print(f'\n{textBef}')
+
+        if textBef not in linesdict:
+        
+            dictionary.write(f'\n{str(textBef)}')
+            #2 ----------------------------------------------------
+            skrin = pyautogui.screenshot(region=(703,355, 655,30))
             skrin.save('lingos.png')
-    
+
             img = cv2.imread('lingos.png')
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        
+
             textBef = pytesseract.image_to_string(img, lang='pol')
             print(f'\n{textBef}')
-    
-            if textBef not in linesdict:
-            
-                dictionary.write(f'\n{str(textBef)}')
-                #2 ----------------------------------------------------
-                skrin = pyautogui.screenshot(region=(703,355, 655,30))
-                skrin.save('lingos.png')
-    
-                img = cv2.imread('lingos.png')
-                img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    
-                textBef = pytesseract.image_to_string(img, lang='pol')
-                print(f'\n{textBef}')
-    
-                dictionary.write(f'{str(textBef)}')
-    
-                pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
-                sleep(0.9)
-    
-                i = i - 1
+
+            dictionary.write(f'{str(textBef)}')
+
+            pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
+            sleep(0.9)
+
+            i = i - 1
+            dictionary.close()
+            dictionary = open("lingos.dict", "r+", encoding='utf8')
+            linesdict = dictionary.readlines()
+        else:
+            pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
+            sleep(0.9)
+            i = i - 1
+
+    else:
+        print(f'\n{textBef}')
+        #playsound(u"oof.mp3")
+        inpt = input('''\n [!]  Słowo nie zostało znalezione w bazie danych
+ [?]  Czy chcesz dodać je do bazy danych?  (T/n)    ''')
+
+        if inpt == 'T' or inpt == 't':
+            try:
+                inpt = str(input('\n [?]  Wpisz słowo po polsku (musi być DOKŁADNIE tak samo jak w lingosie)    '))
+                dictionary.write(f"\n{inpt}")
+                inpt = str(input('\n [?]  Wpisz słowo po angielsku    '))
+                dictionary.write(f"\n{inpt}\n")
+
+                print('\n [*]  Słowo zostało pomyślnie dodane do bazy danych. Wróć do Firefoxa i naduś F1    ')
+                
+                while is_pressed('f1') != True:
+                    continue
+
                 dictionary.close()
                 dictionary = open("lingos.dict", "r+", encoding='utf8')
                 linesdict = dictionary.readlines()
-            else:
-                pyautogui.click(pyautogui.locateOnScreen('dalej.png'))
-                sleep(0.9)
-                i = i - 1
-    
+            except:
+                input('\n [!]  Wystąpił problem z dodaniem słowa do bazy danych. Jeśli błąd będzie się powtarzał, skontaktuj się ze mną na FB lub Discordzie    ')
+                wyjdz()
+
         else:
-            print(f'\n{textBef}')
-            #playsound(u"oof.mp3")
-            inpt = input('''\n [!]  Słowo nie zostało znalezione w bazie danych
-     [?]  Czy chcesz dodać je do bazy danych?  (T/n)    ''')
-    
-            if inpt == 'T' or inpt == 't':
-                try:
-                    inpt = str(input('\n [?]  Wpisz słowo po polsku (musi być DOKŁADNIE tak samo jak w lingosie)    '))
-                    dictionary.write(f"\n{inpt}")
-                    inpt = str(input('\n [?]  Wpisz słowo po angielsku    '))
-                    dictionary.write(f"\n{inpt}\n")
-    
-                    print('\n [*]  Słowo zostało pomyślnie dodane do bazy danych. Wróć do Firefoxa i naduś F1    ')
+            print('\n [*]  W takim razie musisz wpisać i zatwierdzić je ręcznie :(    ')
+            print('\n [*]  Wróć do Firefoxa i naduś F1    ')
+            while is_pressed('f1') != True:
+                continue
 
-                    while is_pressed('f1') != True:
-                        continue
-    
-                    dictionary.close()
-                    dictionary = open("lingos.dict", "r+", encoding='utf8')
-                    linesdict = dictionary.readlines()
-                except:
-                    input('\n [!]  Wystąpił problem z dodaniem słowa do bazy danych. Jeśli błąd będzie się powtarzał, skontaktuj się ze mną na FB lub Discordzie    ')
-                    wyjdz()
-    
-            else:
-                print('\n [*]  W takim razie musisz wpisać i zatwierdzić je ręcznie :(    ')
-                print('\n [*]  Wróć do Firefoxa i naduś F1    ')
-
-                while is_pressed('f1') != True:
-                    continue
-    
-    wyjdz()
-    #--------------------------------------------------------------------------END OF MAIN--------------------------------------------------------------------------
+wyjdz()
+#--------------------------------------------------------------------------END OF MAIN--------------------------------------------------------------------------
