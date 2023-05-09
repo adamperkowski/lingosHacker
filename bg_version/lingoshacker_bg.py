d7ad21fa98393eee1d58df498ebc60fec2e0e3f8
Adam Perkowski <adas1per@protonmail.com>
2023-05-09 21:29:05 +0200

    rozpocznij lekcje

--- bg_version/lingoshacker_bg.py
+++ bg_version/lingoshacker_bg.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+#from time import sleep
 
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 driver_service = Service(executable_path=PATH)
@@ -11,11 +12,13 @@ driver.get("https://lingos.pl/students/learning")
 
 cred = open('cred.txt', 'r')
 credlist = cred.readlines()
-login = driver.find_element(By.NAME, 'login')
-login.send_keys(credlist[0])
-login = driver.find_element(By.NAME, 'password')
-login.send_keys(credlist[1])
+find = driver.find_element(By.NAME, 'login')
+find.send_keys(credlist[0])
+find = driver.find_element(By.NAME, 'password')
+find.send_keys(credlist[1])
 cred.close()
+
+driver.find_element(By.XPATH, "//img[@src='/public/svg/rocket']").click()
 
 while True:
     pass
