21ae1df5dfaedd21b206f0573b5fd155be06db70
Adam Perkowski <adas1per@protonmail.com>
2023-12-01 17:18:10 +0100

    ok

--- bg_version/lingoshacker_bg.py
+++ bg_version/lingoshacker_bg.py
@@ -1,5 +1,5 @@
 from colorama import Fore, init, Style
-print('Lingos Hacker background support BETA.\nUWAGA! Dziala tylko na jedna lekcje.')
+print('Lingos Hacker background support BETA.')
 
 from time import sleep
 sleep(1)
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 from random import choice
+from time import sleep
 
 try:
     dictionary = open("lingos.dict", "r+", encoding='utf8')
@@ -34,7 +35,7 @@ print(f'{Fore.CYAN} [*] Loaded successfully.{Fore.RESET}')                      
 #ilee = int(input(f'{Fore.GREEN} [?] Podaj liczbe slow: {Fore.RESET}'))
 ilee = 20000
 
-driver.get("https://lingos.pl/students/learning")
+driver.get("https://lingos.pl/students/group")
 
 cred = open('cred.txt', 'r')
 credlist = cred.readlines()
@@ -45,45 +46,69 @@ find.send_keys(credlist[1])
 cred.close()
 
 driver.implicitly_wait(8)
-driver.find_element(By.XPATH, "//img[@src='/public/svg/rocket']").click()
-driver.implicitly_wait(8)
+
+if driver.find_element(By.XPATH, "//img[@src='/public/svg/rocket']").size != 0:
+    driver.find_element(By.XPATH, "//img[@src='/public/svg/rocket']").click()
+    driver.implicitly_wait(8)
 
 for i in range(ilee):
     driver.implicitly_wait(1.5)
-
     try:
-        if driver.find_element(By.CSS_SELECTOR, 'h4.m-0').size != 0:
-            textBef = driver.find_element(By.CSS_SELECTOR, 'h3.mb-4').text
-            dictionary.write(f"{driver.find_element(By.CSS_SELECTOR, 'h3.mb-0').text}\n")
-            dictionary.write(f"{textBef}\n")
-
-            dictionary.close()
-            dictionary = open("lingos.dict", "r+", encoding='utf8')
-            linesdict = dictionary.readlines()
-            driver.find_element(By.CSS_SELECTOR, 'button.w-100').click()
-    
-    except:
         try:
-            textBef = str(driver.find_element(By.ID, 'content_flashcard_section').find_element(By.TAG_NAME, 'h3').text + '\n')
-            #textBef = str(driver.find_element(By.CSS_SELECTOR, 'h3.mb-0').text + '\n')
-            if linesdict.count(textBef) == 1:
-                find = driver.find_element(By.ID, 'answer')
-                find.send_keys(linesdict[linesdict.index(textBef)+1])
+            if driver.find_element(By.ID, 'new_teacher_div').find_element(By.CSS_SELECTOR, 'h5.mb-0').size != 0:
+                textBef = driver.find_element(By.ID, 'new_teacher_div').find_element(By.ID, 'new_teacher_main_text').text
+                dictionary.write(f"{driver.find_element(By.ID, 'new_teacher_div').find_element(By.ID, 'new_teacher_additional_text').text}\n")
+                dictionary.write(f"{textBef}\n")
 
-                actions.send_keys(Keys.RETURN).perform()
+                dictionary.close()
+                dictionary = open("lingos.dict", "r+", encoding='utf8')
+                linesdict = dictionary.readlines()
+                driver.find_element(By.CSS_SELECTOR, 'button.w-100').click()
                 driver.implicitly_wait(8)
-            else:
-                indexes = [i for i, x in enumerate(linesdict) if x == textBef]
-                print(indexes)
-                random_index = choice(indexes)
 
-                find = driver.find_element(By.ID, 'answer')
-                find.send_keys(linesdict[random_index+1])
-
-                actions.send_keys(Keys.RETURN).perform()
-                driver.implicitly_wait(8)
         except:
-                input('brak slowa w bazie\n')
+            try:
+                textBef = str(driver.find_element(By.ID, 'flashcard_div').find_element(By.TAG_NAME, 'h3').text + '\n')
+                #textBef = str(driver.find_element(By.CSS_SELECTOR, 'h3.mb-0').text + '\n')
+                if linesdict.count(textBef) == 1:
+                    find = driver.find_element(By.ID, 'flashcard_answer_input')
+                    find.send_keys(linesdict[linesdict.index(textBef)+1])
+
+                    driver.implicitly_wait(7)
+                    actions.send_keys(Keys.RETURN).perform()
+                    sleep(1)
+                    actions.send_keys(Keys.RETURN).perform()
+
+                else:
+                    indexes = [i for i, x in enumerate(linesdict) if x == textBef]
+                    random_index = choice(indexes)
+
+                    find = driver.find_element(By.ID, 'flashcard_answer_input')
+                    find.send_keys(linesdict[random_index+1])
+
+                    actions.send_keys(Keys.RETURN).perform()
+                    sleep(1)
+                    actions.send_keys(Keys.RETURN).perform()
+                    driver.implicitly_wait(7)
+
+            except:
+                    actions.send_keys(Keys.RETURN).perform()
+                    sleep(1)
+
+                    textBef = driver.find_element(By.ID, 'flashcard_div').find_element(By.ID, 'flashcard_error_correct').text
+                    dictionary.write(f"{driver.find_element(By.ID, 'flashcard_div').find_element(By.ID, 'flashcard_main_text').text}\n")
+                    dictionary.write(f"{textBef}\n")
+
+                    dictionary.close()
+                    dictionary = open("lingos.dict", "r+", encoding='utf8')
+                    linesdict = dictionary.readlines()
+                    actions.send_keys(Keys.RETURN).perform()
+                    driver.implicitly_wait(8)
+
+    except:
+        driver.get('https://lingos.pl/s/lesson/0,0,15695')
+
+        sleep(1)
 
 print(f'{Fore.CYAN} [*] Bot zakonczyl dzialanie{Fore.RESET}\nWcisnij dowolny przycisk.')
 input()
